45c0f95a1a2138df445babc93b8ae14793b0da92
spbhasker07 <spbhasker07@gmail.com>
2024-09-15 14:30:29 +0530

    student mangment.py

--- student.py
+++ student.py
@@ -19,12 +19,7 @@ while True:
     print("WELCOME".center(45))
     print("_"*45)
     print("Plsese chose an option")
-    print("plse select operation-\n"\
-      "1.To see student list\n"\
-        "2.to remove data\n"\
-            "3.To serch data\n"\
-            "4.To add new list\n"\
-                "5.exit")
+    print("1. To see student list\n2. To remove data\n3. To search data\n4. To add new list\n5. Exit")
     select=int(input("select  option b/w in 1,2,3,4,5"))
     my_list=["aman","bhasker","ravi","soraj","abhijeet","rohan"]
     if select==1:
