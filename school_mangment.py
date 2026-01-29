3e91e9b61539ab76b4fc71f0a7a0c49092cdf883
sangh priy bhasker <spbhasker07@gmail.com>
2026-01-29 11:53:37 +0530

    Add files via upload

--- school_mangment.py
+++ school_mangment.py
@@ -1,78 +1,70 @@
-print("_"*45)
-print("STUDENT MANGMENT SYSTEM".center(45))
-print("_"*45)
-student_list=["aman","rohit","puja","pinky"]
-def see():
-    try:
-          with open("student.txt", "r") as file:
-              print("cureent student fron list")
-              for line in file:
-                  print(line.strip())
-    except FileNotFoundError:
-        print("No students found in the file.")
+student_recoud={}
 def add():
-    dynamic_dict = {}  # Initialize the dictionary outside the file block
-    dynamic_dict["name"] = input("Enter the name of the student: ")
-    dynamic_dict["age"] = int(input("Enter the age: "))
-    dynamic_dict["city"] = input("Enter the city: ")
-    dynamic_dict["number"] = input("Enter the phone number: ")  # Changed to string to avoid number issues
-    dynamic_dict["id"] = input("Enter the ID proof number: ")
-
-    with open("student.txt", "a") as file:  # Open the file here and append the details
-        file.write(f"{dynamic_dict['name']},{dynamic_dict['age']},{dynamic_dict['city']},{dynamic_dict['number']},{dynamic_dict['id']}\n")
-    print(f"{dynamic_dict['name']} has been added successfully!")
-
-def remove():
-    try:
-        student_to_remove = input("Enter the name of the student to remove: ")
-        with open("student.txt", "r") as file:
-            lines = file.readlines()
-        found = False
-        with open("student.txt", "w") as file:
-            for line in lines:
-                if not line.startswith(student_to_remove):
-                    file.write(line)
-                else:
-                    found=True
-            if found:
-                print(f"{student_to_remove} removed")
-            else:
-                print(f"{student_to_remove} not found")
-    except FileNotFoundError:
-        print("plese add student first")
-
-def serch():
-    try:
-        student_serch = input("Enter the name of the student to search: ")
-        with open("student.txt", "r") as file:
-            found = False
-            for line in file:
-                if line.startswith(student_serch):
-                    print(f"{student_serch} found in the list: {line.strip()}")
-                    found = True
-                    break
-        if not found:
-            print(f"{student_serch} not found in the list.")
-    except FileNotFoundError:
-        print("Student file not found, please add a student first.")
-while True:
-    print("1. To see student list\n2. To add data\n3. To remove data\n4. To serch list\n5. Exit")
-    select=int(input("select  option b/w in 1,2,3,4,5"))
-    if select == 1:
-        see()
-    elif select == 2:
-        add()
-    elif select == 3:
-        remove()
-    elif select == 4:
-        serch()
-    elif select == 5:
-        print("Exiting the program.")
-        break
+    student_recoud["id"]=int(input("enter the id number of student"))
+    student_recoud['name']=input("enter the name of students")
+    student_recoud['class']=int(input("enter the class"))
+    student_recoud['age']=int(input("enter the the age"))
+    student_recoud['course']=input("enter the course ")
+    student_recoud["Fee"]=int(input("enter the fee"))
+    print(student_recoud)
+def fees():
+    id_no = int(input("\nEnter the student ID to check fee: "))
+    if id_no == student_recoud.get("id"):
+        if student_recoud["Fee"] == 5000:
+            print("fee is paid")
+        else:
+            print("fee is not fully paid")
     else:
-        print("Invalid option. Please try again.")
+        print("invalde student")
+def exam():
+    global subject
+    subject = {}
+    number_of_subject = int(input("Enter number of subjects: "))
 
-    ch = input("Do you want to continue (y/n): ").lower()
-    if ch != 'y':
-        print("Exiting the program.")
-        break
+    for i in range(number_of_subject):
+        sub_name = input(f"Enter subject {i+1} name: ")
+        marks = float(input(f"Enter marks of {sub_name}: "))
+        subject[sub_name] = marks
+def result():
+    id_no=int(input("enter the id"))
+    if id_no in student_recoud.get("id"):
+        if student_recoud["Fee"]==5000:
+            print("you are elgible for exam")
+        else:
+            print("need to pay the fees")
+    else:
+        print("ivalde id")
+def mark_sheet():
+    print("="*45)
+    print("MARK SHEET".center(45))
+    print("="*45)
+    total=0
+    for sub,mark in subject.items():
+        print(sub,":",mark)
+        total+=1
+    percentage = total / len(subject)
+    print("Total Marks:", total)
+    print("Percentage:", percentage)
+
+    if percentage >= 40:
+        print("Result: PASS ")
+    else:
+        print("Result: FAIL")
+def see():
+    id_no=int(input("enter the id number"))
+    if id_no in student_recoud.get(id):
+        print(student_recoud)
+print("1.Add the new student \n 2.Fee status \n 3.result \n 4.mark_sheet \n see")
+select_choice=int(input("Enter the choice of the user"))
+if select_choice==1:
+    add()
+elif select_choice==2:
+    fees()
+elif select_choice==3:
+    result()
+elif select_choice==4:
+    mark_sheet()
+elif select_choice==5:
+    see()
+else:
+    print("invade choice")
